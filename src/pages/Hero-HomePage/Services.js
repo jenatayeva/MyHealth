4d9f05fd6e8b55e8357e5661cc526525c2024be5
jenatayeva <atayevajennet1126@gmail.com>
2022-11-13 13:53:18 +0500

    cards are ready

--- src/pages/Hero-HomePage/Services.js
+++ src/pages/Hero-HomePage/Services.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import '../../style/App.scss'
+import chatIcon from '../../imgs/chat.svg'
+import calendarIcon from '../../imgs/calendar.svg'
+import doctorIcon from '../../imgs/doctor.svg'
+import searchIcon from '../../imgs/search.svg'
 
 
 const Services = () => {
@@ -10,11 +14,32 @@ const Services = () => {
           <h3 className='h3-blue'>MyHealth free solution</h3>
           <h1 className='h1-black'>MyHealth free solution</h1>
         </div>
-        <div className='sections'>
+        <div className='services'>
           <div className='card'>
             <div className='inner-card'>
-              <img src='' />
+              <img src={doctorIcon} />
               <h3 className='h3-black'>Search Doctor</h3>
+              <p className='p-gray'>Registration for a check at the clinic needed for treatment</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='inner-card'>
+              <img src={chatIcon} />
+              <h3 className='h3-black'>Request cancellation</h3>
+              <p className='p-gray'>Registration for a check at the clinic needed for treatment</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='inner-card'>
+              <img src={calendarIcon} />
+              <h3 className='h3-black'>Make appointment</h3>
+              <p className='p-gray'>Registration for a check at the clinic needed for treatment</p>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='inner-card'>
+              <img src={searchIcon} />
+              <h3 className='h3-black'>Get Solutions</h3>
               <p className='p-gray'>Registration for a check at the clinic needed for treatment</p>
             </div>
           </div>
