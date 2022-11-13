b85627e10fdcc760250db424d854acf28f0f4303
Parahat <parahat.iljanow@gmail.com>
2022-11-13 21:06:12 +0500

    fix - hospitals

--- src/components/Hospital/HospitalList.js
+++ src/components/Hospital/HospitalList.js
@@ -1,18 +1,34 @@
 import React from 'react'
+import HospitalListItem from './HospitalListItem'
 
+const hospitalsArray = [
+  {
+    title: 'Lorem 1',
+    src: '../../imgs/ene-mahri.svg'
+  },
+  {
+    title: 'Lorem 2',
+    src: '../../imgs/ene-mahri.svg'
+  },
+  {
+    title: 'Lorem 3',
+    src: '../../imgs/ene-mahri.svg'
+  },
+  {
+    title: 'Lorem 4',
+    src: '../../imgs/Garyragoyulmasyz.svg'
+  }
+]
 
-const CardItemSquire = (props) => {
-  const hospitalsArray = [{}]
+const HospitalList = () => {
   return (
-    <div className='card-item'>
-      <div className='card-img'>
-        <img/>
-      </div>
-      <div className='card-title'>
-        <p className='p-black p-bold'></p>
-      </div>
+    <div>
+      <h1>Hospitals</h1>
+      {hospitalsArray.map((item, index) => (
+        <HospitalListItem key={index} item={item} />
+      ))}
     </div>
   )
 }
 
-export default CardItemSquire
+export default HospitalList
---
+++ src/components/Hospital/HospitalListItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const HospitalListItem = ({ item }) => {
+  const { title, src } = item;
+  return (
+    <div className='card-item'>
+      <div className='card-img'>
+        <img src={src} alt={title} />
+      </div>
+      <div className='card-title'>
+        <p className='p-black p-bold'>{title}</p>
+      </div>
+    </div>
+  )
+}
+
+export default HospitalListItem
